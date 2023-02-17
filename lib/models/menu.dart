f9b1f6078e0e3e0abcddd9de9e172179a9ccf99e
Minji Kim <sml07113@gmail.com>
2023-02-17 17:41:00 +0900

    [Feat] Create result screen ui (#4)

---
+++ lib/models/menu.dart
@@ -0,0 +1,11 @@
+class Menu {
+  final String name;
+  final String description;
+  final String image;
+
+  Menu({
+    required this.name,
+    required this.description,
+    required this.image,
+  });
+}
