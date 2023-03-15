9387c263f8d74370f4f95d89ed76025f9070be76
Minji Kim <sml07113@gmail.com>
2023-03-15 21:33:36 +0900

    [Feat] Create camera overlay guide (#13)

--- lib/screens/camera/camera_screen.dart
+++ lib/screens/camera/camera_screen.dart
@@ -39,7 +39,15 @@ class _CameraScreenState extends State<CameraScreen> {
         future: _initializeControllerFuture,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.done) {
-            return CameraPreview(_controller);
+            return AspectRatio(
+              aspectRatio: 1 / _controller.value.aspectRatio,
+              child: Stack(
+                children: [
+                  CameraPreview(_controller),
+                  cameraOverlay(padding: 50, aspectRatio: 0.6, color: const Color(0x55000000)),
+                ],
+              ),
+            );
           } else {
             return const Center(child: CircularProgressIndicator());
           }
@@ -69,4 +77,50 @@ class _CameraScreenState extends State<CameraScreen> {
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
+}
+
+Widget cameraOverlay({required double padding, required double aspectRatio, required Color color}) {
+  return LayoutBuilder(
+    builder: (context, constraints) {
+      double parentAspectRatio = constraints.maxWidth / constraints.maxHeight;
+      double horizontalPadding;
+      double verticalPadding;
+
+      if (parentAspectRatio < aspectRatio) {
+        horizontalPadding = padding;
+        verticalPadding = (constraints.maxHeight - ((constraints.maxWidth - 2 * padding) / aspectRatio)) / 2;
+      } else {
+        verticalPadding = padding;
+        horizontalPadding = (constraints.maxWidth - ((constraints.maxHeight - 2 * padding) * aspectRatio)) / 2;
+      }
+
+      return Stack(
+        fit: StackFit.expand,
+        children: [
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Container(width: horizontalPadding, color: color)),
+          Align(
+            alignment: Alignment.centerRight,
+            child: Container(width: horizontalPadding, color: color)),
+          Align(
+            alignment: Alignment.topCenter,
+            child: Container(
+              margin: EdgeInsets.only(left: horizontalPadding, right: horizontalPadding),
+              height: verticalPadding,
+              color: color)),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              margin: EdgeInsets.only(left: horizontalPadding, right: horizontalPadding),
+              height: verticalPadding,
+              color: color)),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: horizontalPadding, vertical: verticalPadding),
+            decoration: BoxDecoration(border: Border.all(color: ColorPalette.primaryGold)),
+          ),
+        ],
+      );
+    }
+  );
 }
